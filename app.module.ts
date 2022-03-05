7f3bded9dfe57abd1acea1ed8adfcab3ab38a40d
Akshay Kumar <86004396+EfficaciousDevs@users.noreply.github.com>
2022-03-05 08:51:38 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddRestoComponent } from './add-resto/add-resto.component';
+import { UpdateRestoComponent } from './update-resto/update-resto.component';
+import { ListRestoComponent } from './list-resto/list-resto.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddRestoComponent,
+    UpdateRestoComponent,
+    ListRestoComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
